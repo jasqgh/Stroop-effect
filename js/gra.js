07eaa8c282eb375262664161975806595cbdf83b
jasqgh <120196084+jasqgh@users.noreply.github.com>
2022-12-09 16:24:16 +0100

    Add main files

---
+++ js/gra.js
@@ -0,0 +1,78 @@
+
+
+
+( function($) {
+    
+    $(".pudelko").click(function() {
+        $(this).css("display", "none");
+        $('.green').each(function(){
+            $(this).css({"left": Math.random() * window.innerWidth, "top": Math.random() * window.innerHeight}).css("display","block");
+        
+        });
+
+       
+    });
+
+
+} ) ( jQuery );
+
+
+
+
+
+
+
+
+/**( function($) {
+    $(".pudelko").click(function() {
+        $(this).css("display", "none");
+        $('.boxy').css("display", "block");
+        
+    });
+} ) ( jQuery );
+
+
+
+
+
+
+
+( function($) {
+    
+    $(".pudelko").click(function() {
+        $(this).css("display", "none");
+        $('.red').each(function(){
+            $(this).css({"left": Math.random() * window.innerWidth, "top": Math.random() * window.innerHeight}).css("display","block");
+        
+        });
+
+       
+    });
+
+
+} ) ( jQuery );
+
+
+
+/**( function($) {
+    $(".pudelko").click(function() {
+        $(this).css("display", "none");
+        green.css("display", "block");
+        
+    });
+} ) ( jQuery );
+
+
+
+
+
+
+
+/**( function($) {
+    $(".pudelko").click(function() {
+        $(this).css("display", "none");
+    });
+} ) ( jQuery );**/
+
+
+
